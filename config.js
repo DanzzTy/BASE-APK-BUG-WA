6d882c03d2d26e5532320d7f3836e9fd77191131
Danzz <danishcoding205@gmail.com>
2026-06-18 15:12:46 +0700

    Create config.js with bot and API settings
    Add configuration settings for Telegram bot and WhatsApp API.

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  TELEGRAM_BOT_TOKEN: "8810275629:AAGeDPr9WfM9jsLrb0Yz6AZLVrnG4ynKMDc",
+  OWNER_TELEGRAM_ID: 6143435003,
+  DEFAULT_OWNER_USERNAME: "pepet", // username owner buat login web
+  DEFAULT_OWNER_PASSWORD: "123", // password owner buat login web (bisa diubah di sini)
+
+  // URL Base dashboard website untuk menghasilkan tautan akses akun
+  DASHBOARD_URL: "http://localhost:3000", // ganti pake link panel + port 
+
+  // WhatsApp API Server configuration
+  WA_SERVER_PORT: 3000,
+  WA_SERVER_URL: "http://localhost:3000", // ini juga
+
+  CREDITS: ["Vanness Wangsaff", "Baileys"]
+};
